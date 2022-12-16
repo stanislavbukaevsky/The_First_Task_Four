06a6048b109b5e4d401e659d6333658543eaf109
stanislavi18061989 <96617178+stanislavbukaevsky@users.noreply.github.com>
2022-12-16 14:39:54 +0300

    Первый коммит

---
+++ src/main/java/ru/hogwarts/schoolsix/SchoolSixApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.schoolsix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolSixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolSixApplication.class, args);
+	}
+
+}
